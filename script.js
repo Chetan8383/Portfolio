f510fa350a0f74c479cdc29251d4c619909f4648
Chetan Mehra <157389143+Chetan8383@users.noreply.github.com>
2024-07-07 17:43:59 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -57,6 +57,47 @@ window.onload = function () {
     document.body.appendChild(css);
 };
 
-// ////////////////////////////////////////////////////END//    
+// ////////////////////////////////////////////////////END//
 
 /////////////////////////CURSOR///////////////////////
+
+const form = document.getElementById('form');
+const result = document.getElementById('result');
+
+form.addEventListener('submit', function (e) {
+    const formData = new FormData(form);
+    e.preventDefault();
+
+    const object = Object.fromEntries(formData);
+    const json = JSON.stringify(object);
+
+    result.innerHTML = "Please wait..."
+
+    fetch('https://api.web3forms.com/submit', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: json
+    })
+        .then(async (response) => {
+            let json = await response.json();
+            if (response.status === 200) {
+                result.innerHTML = json.message;
+            } else {
+                console.log(response);
+                result.innerHTML = json.message;
+            }
+        })
+        .catch(error => {
+            console.log(error);
+            result.innerHTML = "Something went wrong!";
+        })
+        .then(function () {
+            form.reset();
+            setTimeout(() => {
+                result.style.display = "none";
+            }, 3000);
+        });
+});
